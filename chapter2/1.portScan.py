72aebc2ee1f97d89311f17f7c29b66106160c7f5
David Higgins <thehig@gmail.com>
2015-07-27 19:20:03 +0100

    Added portScanners

--- chapter2/1.portScan.py
+++ chapter2/1.portScan.py
@@ -1,15 +1,26 @@
 import optparse
-
 from socket import *
+from threading import *
+
+screenLock = Semaphore(value=1)
 
 def connScan(tgtHost, tgtPort):
 	try:
 		connSkt = socket(AF_INET, SOCK_STREAM)
 		connSkt.connect((tgtHost, tgtPort))
+		connSkt.send('ViolentPython\r\n')
+		results = connSkt.recv(100)
+
+		screenLock.acquire()
 		print '[+] %d/tcp open' % tgtPort
-		connSkt.close()
+		print '[+] ' + str(results)
 	except:
+		screenLock.acquire()
 		print '[-] %d/tcp closed' % tgtPort
+	finally:
+		screenLock.release()
+		connSkt.close()
+
 
 def portScan(tgtHost, tgtPorts):
 	try:
@@ -22,31 +33,33 @@ def portScan(tgtHost, tgtPorts):
 		tgtName = gethostbyaddr(tgtIP)
 		print '\n[+] Scan Results for: ' + tgtName[0]
 	except:
-		print '\n[-] Scan Results for: ' + tgtIP
+		print '\n[+] Scan Results for: ' + tgtIP
 
 	setdefaulttimeout(1)
 
 	for tgtPort in tgtPorts:
-		print '[*] Scanning Port ' + tgtPort
-		connScan(tgtHost, int(tgtPort))
+		t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+		t.start()
 
 
 def main():
 	parser = optparse.OptionParser('%prog -H <target host> -p <target port>')
 
 	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-	parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+	parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
 
 	(options, args) = parser.parse_args()
 
 	tgtHost = options.tgtHost
-	tgtPort = options.tgtPort
 
-	if (tgtHost == None) | (tgtPort == None):
+	tgtPorts = str(options.tgtPort).split(',')
+
+	if (tgtHost == None) | (tgtPorts[0] == None):
+		print "[-] You must specify a target host and port[s]."
 		print parser.usage
 		exit(0)
 
-	connScan(tgtHost, tgtPort)
+	portScan(tgtHost, tgtPorts)
 
 if __name__ == "__main__":
 	main()
